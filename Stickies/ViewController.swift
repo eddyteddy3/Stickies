a5fd05f86f5b92c48b5fe9eb57d8735550ab0555
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-01-26 23:41:43 +0500

    Initial Commit

---
+++ Stickies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stickies
+//
+//  Created by Moazzam Tahir on 26/01/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
