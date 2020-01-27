8fdc66d115a00174e2c28fb514d5dad5848ef5cf
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-01-27 21:12:28 +0500

    initialized the basic view

--- Stickies/ViewController.swift
+++ Stickies/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Outlet Connections
+    @IBOutlet var bottomView: UIView!
+    @IBOutlet var cameraView: UIView!
+    @IBOutlet var cameraShutterButton: UIButton!
+    @IBOutlet var flipCamera: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initializeUIView() //initializing the view
     }
 
+    
+    func initializeUIView() {
+        
+        //camera view UI modifications
+        cameraView.backgroundColor = UIColor.init(named: "modifiedDarkYellow")
+        
+        //bottom view UI modifications
+        bottomView.addCorner(cornerRadius: 20)
+        bottomView.backgroundColor = UIColor.init(named: "modifiedGray")
+        bottomView.addDropShadow(color: .black, radius: 6, shadowOffset: .init(width: 0, height: 8), shadowOpacity: 0.8)
+    }
 
 }
 
+extension UIView {
+    //to add drop down shadow to any specific view
+    func addDropShadow(color: UIColor, radius: CGFloat, shadowOffset: CGSize, shadowOpacity: Float) {
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowRadius = radius
+        self.layer.shadowOffset = shadowOffset
+        self.layer.shadowOpacity = shadowOpacity
+    }
+    
+    //to add the corner to any particular view
+    func addCorner(cornerRadius: CGFloat) {
+        self.layer.cornerRadius = cornerRadius
+        //self.layer.cornerCurve
+    }
+}
