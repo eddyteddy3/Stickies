d703bef4d9f71e3f8ea5822eb75b138739504964
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-01-30 21:22:12 +0500

    translation gesture added

--- Stickies/Controller/StickerViewController.swift
+++ Stickies/Controller/StickerViewController.swift
@@ -27,24 +27,41 @@ class StickerViewController: UIViewController {
         initializeIU()
     }
     
-    //adding gestures to the imageView for Drag and Pinch in & out
-    func addGestures() {
-        let pinchGesture = UIPinchGestureRecognizer.init()
-        pinchGesture.addTarget(self, action: #selector(scaleImage(_:)))
-        croppedImage.addGestureRecognizer(pinchGesture)
-        croppedImage.isUserInteractionEnabled = true //enabling to make gesture functional
-    }
-    
-    @objc
-    func scaleImage(_ sender: UIPinchGestureRecognizer) {
-        self.croppedImage.transform = CGAffineTransform.init(scaleX: sender.scale, y: sender.scale)
-    }
-    
     //initializing the uiView
     func initializeIU() {
         stickerEditingView.addCorner(cornerRadius: 20)
         backgroundView.addCorner(cornerRadius: 20)
         backgroundView.addDropShadow(color: .black, radius: 8, shadowOffset: .init(width: 0, height: 4), shadowOpacity: 0.4)
+    }
+    
+    //adding gestures to the imageView for Drag and Pinch in & out
+    func addGestures() {
+        //adding pinch gesture to scale the image
+        let pinchGesture = UIPinchGestureRecognizer.init()
+        pinchGesture.addTarget(self, action: #selector(scaleImage(_:)))
+        
+        //adding pan gesture to drag the image
+        let panGesture = UIPanGestureRecognizer.init(target: self, action: #selector(dragImage(_:)))
+        
+        croppedImage.addGestureRecognizer(pinchGesture)
+        croppedImage.addGestureRecognizer(panGesture)
+        croppedImage.isUserInteractionEnabled = true //enabling to make gesture functional
+        
+    }
+    
+    //function to drag the image from the recieving event
+    @objc
+    func dragImage(_ sender: UIPanGestureRecognizer) {
+        print(sender.translation(in: backgroundView))
+        let translation = sender.translation(in: self.backgroundView)
+        croppedImage.center = CGPoint.init(x: croppedImage.center.x + translation.x, y: croppedImage.center.y + translation.y)
+        sender.setTranslation(.zero, in: self.backgroundView)
+    }
+    
+    //function to scale the image
+    @objc
+    func scaleImage(_ sender: UIPinchGestureRecognizer) {
+        self.croppedImage.transform = CGAffineTransform.init(scaleX: sender.scale, y: sender.scale)
     }
 
 }
